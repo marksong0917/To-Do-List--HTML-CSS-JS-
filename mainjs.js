f790b390ccd9110e2107ee231f7674db98d4e1ba
marksong0917 <54914202+marksong0917@users.noreply.github.com>
2019-11-18 15:18:26 -0500

    Add files via upload

--- mainjs.js
+++ mainjs.js
@@ -1,73 +1,66 @@
+// JavaScript - To Do List - Mark Song - 11/18/19
+
 // When the html button, add new item is clicked, calls this function to add a new Element
+
 function addNew() {
   // Assign newTaskValue of the value of element by id 'newTask', myList of  Master list; ul  , creating new element li and checkbox input
   var newTaskValue = document.getElementById("newTask").value;
   var myList = document.getElementById("todoList"); //li, the list
   var newTaskLi = document.createElement("li");
   var checkbox = document.createElement("input"); // create the checkbox element
-  // giving the checkbox a type of checkbox and a class name of checkboxes
+  // creating elements checkbox and a delete button and assigning them a type, classname and id
   checkbox.type = "checkbox";
   checkbox.className = "checkboxes";
+  var bdelete = document.createElement("button"); // delete button
+  bdelete.id = "b-delete";
   //appending the checkbox and new task's value under the li element
   newTaskLi.appendChild(checkbox);
   newTaskLi.appendChild(document.createTextNode(newTaskValue));
+  newTaskLi.appendChild(bdelete);
   //appending the new li element inside the master ul element and logging it to console
   myList.appendChild(newTaskLi);
+  checkbox.addEventListener("click", highLight);
+  bdelete.addEventListener("click", removeItem);
   console.log("Item and Checkbox Added Successfully");
 }
-// above code no error
 
-//Doesn't work
-// Selecting every element inside the li element
-// declaring a for loop starting from 0 to the length of myList (li)
-//creating a complete button and assigning them a classname of b-delete and adding a textnode of X
-//Appending a delete button to every element of li
-var myList = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < myList.length; i++) {
-  var completeButton = document.createElement("button");
-  var deleteText = document.createTextNode("X");
-  completeButton.className = "b-delete";
-  completeButton.addEventListener("click", removeItem);
-  completeButton.appendChild(deleteText);
-  myList[i].appendChild(completeButton);
-}
-
-////Doesn't work
+//function removeItem
 // If a delete button is clicked it will delete the parent's element
 function removeItem() {
-  var deleteTask = document.getElementsByClassName("b-delete");
-  var i;
-  for (i = 0; i < deleteTask.length; i++) {
-    deleteTask[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
+  // assigning the parent node to li and li's parent node = ul
+  //remove the element display styles
+  var TaskLi = this.parentNode;
+  var myList = TaskLi.parentNode;
+  var div = this.parentElement;
+  div.style.display = "none";
+  myList.removeChild(TaskLi);
 }
 
-//selecting every checkbox under the classname of "checkboxes"
-//if checkboxes isn't empty, listen for a event listen click and highlight them
-var myList = document.getElementsByTagName("li");
-var j;
-for (j = 0; j < myList.length; j++) {
-  var checkboxes = document.getElementsByClassName("checkboxes");
-  if (checkboxes != null) {
-    console.log("checkbox is null??");
-    console.log(checkboxes);
-    checkboxes[j].addEventListener("click", highLight);
-  }
-}
-
+//function highlight
 // highlight the checkboxes that are checked and make them style = line through it
 function highLight(event) {
   console.log("highlight event entered");
-  if (checkboxes[j].checked) {
-    var list = document.getElementById("todoList"); //li, the list
-    list.style.textDecoration = "through";
-    console.log("Detected Check!");
-  } else {
-    var list = document.getElementById("todoList"); //li, the list
-    list.style.textDecoration = "none";
+  // assigning the parent node to li and li's parent node = ul
+  var TaskLi = this.parentNode;
+  var myList = TaskLi.parentNode;
+  // if the li's children node is CHECKED, HIGHLIGHT through css
+  if (TaskLi.childNodes[0].checked == true) {
+    console.log("highlight checked");
+    //re appends the item to under the list
+    myList.append(TaskLi);
+    TaskLi.style.textDecoration = "line-through";
+  }
+  //if the children node is UNCHECKED. Do not highlight and do nothing
+  else {
+    //re append the item to the list but to the top of the list
+    console.log("highlight unchecked");
+    myList.prepend(TaskLi);
+    TaskLi.style.textDecoration = "none";
   }
 }
+
+// TO DO LIST
+// Style the delete button
+// style the li
+// style the ui format
+//style the checkbox
